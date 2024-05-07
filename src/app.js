cbd04428eb02287b40225de981ad9dc34cdda49f
SunnySingh <beena.webframez@gmail.com>
2024-05-07 09:36:54 +0530

    util added

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ import CookieParser from "cookieparser";
 
 const app = express()
 
-app.use(cors({
+app.use(cors({   // app.use is used when we have to do configuration or to set middleware
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended}))
+app.use(express.urlencoded({extended, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(CookieParser())
 
 export { app } 
